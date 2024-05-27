c377f076aa277f2fec5414bc34537c5499492360
Facundo Lasserre <facundolasserre@MacBook-Pro-de-Facundo.local>
2024-05-27 09:21:30 -0300

    keyboard driver

---
+++ Kernel/include/keyboardDriver.h
@@ -0,0 +1,9 @@
+#ifndef KEYBOARD_DRIVER_H
+#define KEYBOARD_DRIVER_H
+#include <naiveConsole.h>
+
+char getCharCode(int key);
+void keyboard_handler();
+
+
+#endif
---
+++ Kernel/keyboardDriver.c
@@ -0,0 +1,41 @@
+#include <keyboardDriver.h>
+
+
+
+#define PRESS 1
+#define RELEASE 2
+#define ERROR -1
+
+//#define BUFF_LEN 30
+//static int buffSize=0; //cantidad de elementos del buffer
+static int ridx=0; //posicion de escritura
+static int widx=0; //posicion de lectura
+
+#define LEFT_SHIFT 0x2A
+#define RIGHT_SHIFT 0x36
+#define CAPS_LOCK 0x3A
+#define CTRL 0x1D
+
+//static char buffer[BUFF_LEN]={0};
+
+static int shift = 0;
+static int ctrl = 0;
+static int capsLock = 0;
+
+
+static char ScanCodes[256]={0, 0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',
+'\b', '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n', 0, 'a', 's', 
+'d', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', 0, '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 0, 0 };
+
+
+
+char getCharCode(int key){
+    return ScanCodes[key];
+}
+
+void keyboard_handler(){
+    while(1){
+        char c = getCharCode(get_key());
+        ncPrintChar(c);
+    }
+}
